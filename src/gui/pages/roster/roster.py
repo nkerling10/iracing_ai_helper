995a65e678948dc5a77319a9ac55e978f811b888
nkerling10 <nkerling10@gmail.com>
2025-01-04 23:44:09 -0600

    switched to venv format

--- src/gui/pages/roster/roster.py
+++ src/gui/pages/roster/roster.py
@@ -25,114 +25,108 @@ class Driver:
         self.strategy = driver["strategyRiskiness"]
 
 
-class Roster:
-    @staticmethod
-    def _build_headers() -> list:
-        return [
-            "Car",
-            "Name",
-            "Age",
-            "Skill",
-            "Aggr.",
-            "Opt.",
-            "Smth.",
-            "Pitcrew",
-            "Strat.",
-        ]
+def _roster_file_headers() -> list:
+    return [
+        "Car",
+        "Name",
+        "Age",
+        "Skill",
+        "Aggr.",
+        "Opt.",
+        "Smth.",
+        "Pitcrew",
+        "Strat.",
+    ]
 
-    @staticmethod
-    def _build_driver_table(driver_objs: list) -> list:
-        driver_data = []
 
-        for obj in driver_objs:
-            if "NODRIVER" not in obj.name:
-                driver_data.append(
-                    [
-                        obj.car,
-                        obj.name,
-                        obj.age,
-                        obj.skill,
-                        obj.aggression,
-                        obj.optimism,
-                        obj.smoothness,
-                        obj.pitcrew,
-                        obj.strategy,
-                    ]
-                )
+def build_driver_table(driver_objs: list) -> list:
+    driver_data = []
 
-        return driver_data
+    for obj in driver_objs:
+        if "NODRIVER" not in obj.name:
+            driver_data.append(
+                [
+                    obj.car,
+                    obj.name,
+                    obj.age,
+                    obj.skill,
+                    obj.aggression,
+                    obj.optimism,
+                    obj.smoothness,
+                    obj.pitcrew,
+                    obj.strategy,
+                ]
+            )
 
-    @staticmethod
-    def _format_drivers(drivers: list) -> list:
-        driver_objs = []
-        for driver in drivers:
-            driver_objs.append(Driver(driver))
+    return driver_data
 
-        return driver_objs
 
-    @classmethod
-    def build_driver_display_info(cls, roster_path: str) -> list:
-        with open(roster_path, "r") as file:
-            drivers = json.loads(file.read()).get("drivers")
-        driver_objs = cls._format_drivers(drivers)
-        driver_data = cls._build_driver_table(driver_objs)
+def build_driver_display_info(roster_path: str) -> list:
+    with open(roster_path, "r") as roster_file:
+        driver_objs = [Driver(driver) for driver in 
+                       json.loads(roster_file.read()).get("drivers")]
+    driver_data = build_driver_table(driver_objs)
 
-        return driver_data
+    return driver_data
 
-    @classmethod
-    def build_layout(cls, driver_data: list) -> list:
-        headers = cls._build_headers()
-        roster_tab_layout = [
-            [
-                sg.Table(
-                    values=driver_data,
-                    headings=headers,
-                    justification="center",
-                    key="-TABLE-",
-                    num_rows=35,
-                )
-            ],
-            [
-                sg.Text(text="Track: "),
-                sg.Input(key="-TRACK-", enable_events=True, size=(15, None)),
-                sg.Text(key="-TRACKSTATUS-"),
-            ],
-            [sg.Button("Randomize"), sg.Button("Copy")],
-        ]
 
-        season_tab_layout = [[]]
-        standings_tab_layout = [[]]
+def build_roster_tab_layout(driver_data: list) -> list:
+    return [
+        [
+            sg.Table(
+                values=driver_data,
+                headings=_roster_file_headers(),
+                justification="center",
+                key="-TABLE-",
+                num_rows=35,
+            )
+        ],
+        [
+            sg.Text(text="Track: "),
+            sg.Input(key="-TRACK-", enable_events=True, size=(15, None)),
+            sg.Text(key="-TRACKSTATUS-"),
+        ],
+        [sg.Text(text="File loaded:"), sg.Text(key="-ROSTERFILELOADED-")],
+        [sg.Button("Randomize"), sg.Button("Copy")]
+    ]
 
-        layout = [[sg.TabGroup([[sg.Tab("Roster", roster_tab_layout, key="-rostertab-"),
-                                 sg.Tab("Season", season_tab_layout, key="-seasontab"),
-                                 sg.Tab("Standings", standings_tab_layout, key="-standingstab-")]],
-                                 key="-tabgroup1-", tab_location="topleft"),]]
+def build_layout(driver_data: list) -> list:
+    roster_tab_layout = build_roster_tab_layout(driver_data)
 
-        return layout
+    season_tab_layout = [[]]
+    standings_tab_layout = [[]]
 
-    @classmethod
-    def main_window(cls) -> None:
-        driver_data = cls.build_driver_display_info(local_roster_path)
-        layout = cls.build_layout(driver_data)
-        window = sg.Window(
-            "NSK AI Roster Randomizer - Alpha v0.1", layout, finalize=True
-        )
-        while True:
-            event, values = window.read(timeout=1000)
-            if event in (sg.WIN_CLOSED, None, "Exit"):
-                break
-            elif event == "Randomize":
-                if values["-TRACK-"] != "":
-                    randomizer.main(values["-TRACK-"], local_roster_path)
-                    window["-TRACKSTATUS-"].update("Success!")
-                    driver_data = cls.build_driver_display_info(local_roster_path)
-                    window["-TABLE-"].update(values=driver_data)
-                    window.read(timeout=1500)
-                    window["-TRACKSTATUS-"].update("")
-                else:
-                    window["-TRACKSTATUS-"].update("ERROR! Track missing")
-            elif event == "Copy":
-                randomizer.perform_copy(local_roster_path)
+    layout = [[sg.TabGroup([[sg.Tab("Roster", roster_tab_layout, key="-rostertab-"),
+                                sg.Tab("Season", season_tab_layout, key="-seasontab"),
+                                sg.Tab("Standings", standings_tab_layout, key="-standingstab-")]],
+                                key="-tabgroup1-", tab_location="topleft"),]]
+
+    return layout
+
+
+def main_window() -> None:
+    driver_data = build_driver_display_info(local_roster_path)
+    layout = build_layout(driver_data)
+    window = sg.Window(
+        "NSK AI Roster Randomizer - Alpha v0.1", layout, finalize=True
+    )
+    window["-ROSTERFILELOADED-"].update(value=local_roster_path)
+    while True:
+        event, values = window.read(timeout=1000)
+        if event in (sg.WIN_CLOSED, None, "Exit"):
+            break
+        elif event == "Randomize":
+            if values["-TRACK-"] != "":
+                randomizer.main(values["-TRACK-"], local_roster_path)
+                window["-TRACKSTATUS-"].update("Success!")
+                driver_data = build_driver_display_info(local_roster_path)
+                window["-TABLE-"].update(values=driver_data)
+                window.read(timeout=1500)
+                window["-TRACKSTATUS-"].update("")
+            else:
+                window["-TRACKSTATUS-"].update("ERROR! Track missing")
+        elif event == "Copy":
+            randomizer.perform_copy(local_roster_path)
 
 
 if __name__ == "__main__":
@@ -146,4 +140,4 @@ if __name__ == "__main__":
     )
     local_roster_path = settings["PATHS"]["LOCAL_ROSTER"]
     iracing_ai_roster_path = settings["PATHS"]["AI_ROSTER_FOLDER"]
-    Roster.main_window()
+    main_window()
