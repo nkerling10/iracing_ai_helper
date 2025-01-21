60531a33ea5217fab9cd042f1f830afe3ddef9a8
nkerling10 <nkerling10@gmail.com>
2025-01-21 00:50:17 -0600

    working on owner points qual lock in

--- src/gui/functions/race_manager/services/session/qualifying_service.py
+++ src/gui/functions/race_manager/services/session/qualifying_service.py
@@ -6,34 +6,76 @@ import time
 
 ## Local imports
 
+logger = logging.getLogger(__name__)
 
 class QualifyingService:
     @staticmethod
     def _set_field_size(race_manager) -> None:
-        for position in race_manager.ir["SessionInfo"]["Sessions"][
-            race_manager.qualifying_session_num
-        ]["ResultsPositions"]:
-            if position["Position"] == 1:
-                race_manager.race_weekend.pole_winner = [
-                    driver["UserName"]
-                    for driver in race_manager.ir["DriverInfo"]["Drivers"]
-                    if driver["CarIdx"] == position["CarIdx"]
-                ][0]
-                logging.info(
-                    f"Congrats to {race_manager.race_weekend.pole_winner} for winning the pole this week."
-                )
-            if position["Position"] > race_manager.race_settings.field_size:
-                match = [
+        qualifying_results = []
+        cars_in_race = []
+        cars_to_dq = []
+
+        qual_results_raw = race_manager.ir["SessionInfo"]["Sessions"][
+            race_manager.qualifying_session_num]["ResultsPositions"]
+
+        ## Filter out cars that are not running this week and are already disqualified
+        for position in qual_results_raw:
+            driver = [driver["UserName"]
+                      for driver in race_manager.ir["DriverInfo"]["Drivers"]
+                      if driver["CarIdx"] == position["CarIdx"]][0]
+            if "NODRIVER" not in driver:
+                qualifying_results.append(position)
+
+        race_manager.race_weekend.pole_winner = [driver["UserName"]
+                                                 for driver in race_manager.ir["DriverInfo"]["Drivers"]
+                                                 if driver["CarIdx"] == qualifying_results[0]["CarIdx"]][0]
+        logging.info(
+            f"Congrats to {race_manager.race_weekend.pole_winner} for winning the pole this week."
+        )
+
+        ## Example 38 car field = set 1-33
+        for position in range(1, race_manager.race_settings.field_size - 5):
+            cars_in_race.append([
                     driver["CarNumber"]
                     for driver in race_manager.ir["DriverInfo"]["Drivers"]
-                    if driver["CarIdx"] == position["CarIdx"]
-                    and "NODRIVER" not in driver["UserName"]
-                ]
-                if match:
-                    logging.info(f"{match[0]} car missed the race")
-                    race_manager.send_iracing_command(
-                        f"!dq {match[0]} #{match[0]} missed the race"
-                    )
+                    if driver["CarIdx"] == qualifying_results[position]["CarIdx"]
+                ][0])
+            logger.debug(f"{[
+                    driver["UserName"], driver["CarNumber"]
+                    for driver in race_manager.ir["DriverInfo"]["Drivers"]
+                    if driver["CarIdx"] == position["CarIdx"]][0]} is locked in")
+
+        # 38 car field = set 34-37
+        non_locked_in_qual_cars = qualifying_results[race_manager.race_settings.field_size - 4:]
+        cars_drivers_left = []
+        for position in non_locked_in_qual_cars:
+            cars_drivers_left.append([(driver["CarNumber"], driver["Username"]) for driver in 
+                                       race_manager.ir["DriverInfo"]["Drivers"] if driver["CarIdx"]
+                                       == position["CarIdx"]][0])
+        
+        for car_driver in cars_drivers_left:
+            car_num = car_driver[0]
+            driver = car_driver[1]
+
+
+
+        ## final car in the field
+        # 1. past series champion
+        # 2. race winner from the previous or current season
+        # 3. fifth owner points provisional
+
+        for car in cars_to_dq:
+            match = [
+                driver["CarNumber"]
+                for driver in race_manager.ir["DriverInfo"]["Drivers"]
+                if driver["CarNumber"] == car
+                and "NODRIVER" not in driver["UserName"]
+            ]
+            if match:
+                logging.info(f"{match[0]} car missed the race")
+                race_manager.send_iracing_command(
+                    f"!dq {match[0]} #{match[0]} missed the race"
+                )
 
     @classmethod
     def qualifying(cls, race_manager) -> None:
